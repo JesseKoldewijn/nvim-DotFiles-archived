6e0db3599a7631bbdf7603c6e0653edae1b33b6b
Jesse Koldewijn <60965908+JesseKoldewijn@users.noreply.github.com>
2023-03-07 19:04:24 +0000

    init: Initial codebase creation

---
+++ init.lua
@@ -0,0 +1,5 @@
+require("shiftcode.packer")
+require("shiftcode.remap")
+
+-- current-options: dracula & nitepal
+vim.cmd[[colorscheme nitepal]]
---
+++ lua/shiftcode/remap.lua
@@ -0,0 +1,17 @@
+-- requires
+local telscope = require('telescope.builtin')
+
+-- Leader is spacebar (empty-space)
+vim.g.mapleader = " "
+
+-- Keymaps
+vim.keymap.set("n", "<C-s>", vim.cmd.w) -- Write file
+vim.keymap.set("n", "<C-q>", vim.cmd.q) -- Close neovim
+
+-- Keymaps - Plugins
+-- # Telescope - open on spacebar
+vim.keymap.set("n", "<leader>t", telscope.git_files, {}) -- Find file (single-fuzzy)
+vim.keymap.set("n", "<leader>", telscope.find_files, {}) -- Find files (global-fuzzy)
+vim.keymap.set("n", "<leader>s", function() 
+	telscope.grep_string({ search = vim.fn.input("Grep > ") });
+end)
