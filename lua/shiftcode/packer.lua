e4febef6990190a01afe506f25472911178b2769
Jesse Koldewijn <jesse@iungo.nl>
2023-03-08 12:10:02 +0100

    fix: small formatting tweak

--- lua/shiftcode/packer.lua
+++ lua/shiftcode/packer.lua
@@ -13,7 +13,7 @@ return require('packer').startup(function(use)
   	'nvim-telescope/telescope.nvim', tag = '0.1.1',
 	-- or                            , branch = '0.1.x',
   	requires = { {'nvim-lua/plenary.nvim'} }
-      }	
+  }	
 
   -- themes
   use { 'jascha030/nitepal.nvim' }
